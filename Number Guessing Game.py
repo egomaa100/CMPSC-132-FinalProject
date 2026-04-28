cef61e6b9cfab98155dd695e0d8689b45ad1e1db
egomaa100 <167141763+egomaa100@users.noreply.github.com>
2026-04-28 13:24:02 -0400

    difficulty set

--- Number Guessing Game.py
+++ Number Guessing Game.py
@@ -8,5 +8,32 @@ class NumberGuessingGame:
         self.max_attempts = None
         self.difficulty = ""
         self.game_over = False
-        
+    
+    def choose_difficulty(self):
+        valid_choice = False
+        while not valid_choice:
+            print("\nChoose Difficulty:")
+            print("1. Easy (1-50, Unlimited attempts)")
+            print("2. Medium (1-100, 10 attempts)")
+            print("3. Hard (1-200, 5 attempts)")
+
+            choice = input("Enter choice (1-3): ".strip()
+
+            if choice == "1":
+                self.number = random.randint(1,50)
+                self.max_attempts = None
+                self.difficulty = "Easy"
+                valid_choice = True
+            elif choice == "2":
+                self.number = random.randint(1,100)
+                self.max_attempts = 10
+                self.difficulty = "Medium"
+                valid_choice = True
+            elif choice == "3":
+                self.number = random.randint(1,200)
+                self.max_attempts = 5
+                self.difficulty = "Hard"
+                valid_choice = True
+            else:
+                print("Invalid choice. Please select 1, 2, or 3.")
         
