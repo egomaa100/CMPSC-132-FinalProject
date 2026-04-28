d760beefbf732fa07388bf8bd8ea7e1d8d2be6a9
egomaa100 <167141763+egomaa100@users.noreply.github.com>
2026-04-28 12:56:10 -0400

    Add files via upload

---
+++ Number Guessing Game.py
@@ -0,0 +1,9 @@
+#Number Guessing Game
+import random
+
+class NumberGuessingGame:
+    def __init__(self):
+        self.number = 0
+        self.attempts = 0
+        self.max_attempts = None
+        self.difficulty = ""
